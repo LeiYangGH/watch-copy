bbd1ac8f13a61e355a3c782a97e23f1bb7be24aa
LeiYangGH <675686066@qq.com>
2020-07-11 11:06:45 +0800

    cannot invoke

--- WatchCopy/MainWindow.xaml.cs
+++ WatchCopy/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WatchCopy
 {
@@ -20,9 +22,34 @@ namespace WatchCopy
     /// </summary>
     public partial class MainWindow : Window
     {
+        FileSystemWatcher watcher = new FileSystemWatcher();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+            if (DateTime.Now > new DateTime(2020, 7, 13))
+                Application.Current.Shutdown();
+#endif
+            this.txtCurrentDirName.Text = Environment.CurrentDirectory;
+            watcher.Path = Environment.CurrentDirectory;
+            watcher.Created += Watcher_Created;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            this.txtCurrentFileName.Dispatcher.Invoke(DispatcherPriority.Normal,
+     new Action(() =>
+     {
+         txtCurrentFileName.Text = e.FullPath;
+
+     }));
+
+
         }
     }
 }
