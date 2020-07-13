60d66a92e60fa9f4aa7bdf2c217df6c08b599c17
yang.lei <yang.lei@nuance.com>
2020-07-13 08:56:25 +0800

    copy changed

--- WatchCopy/MainWindow.xaml.cs
+++ WatchCopy/MainWindow.xaml.cs
@@ -31,21 +31,45 @@ namespace WatchCopy
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 #if DEBUG
-            if (DateTime.Now > new DateTime(2020, 7, 13))
+            if (DateTime.Now > new DateTime(2020, 7, 15))
                 Application.Current.Shutdown();
 #endif
             this.txtCurrentDirName.Text = Environment.CurrentDirectory;
             watcher.Path = Environment.CurrentDirectory;
             watcher.Created += Watcher_Created;
+            watcher.Changed += Watcher_Changed;
             watcher.EnableRaisingEvents = true;
         }
 
-        private void CopyFile(string srcFullName)
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
         {
-            string srcDir = Path.GetDirectoryName(srcFullName);
+            string srcFullName = e.FullPath;
+            if (srcFullName.Contains("_bak_"))
+                return;
+            if (new FileInfo(srcFullName).Length <= 0)
+                return;
+            this.txtCurrentFileName.Dispatcher.Invoke(DispatcherPriority.Normal,
+     new Action(() =>
+     {
+         txtCurrentFileName.Text = srcFullName;
+     }));
+            string desFullName = ConstructDestFullName(srcFullName, "changed");
+
+            CopyFile(srcFullName, desFullName);
+        }
+
+        private string ConstructDestFullName(string srcFullName, string action)
+        {
+            //string srcDir = Path.GetDirectoryName(srcFullName);
             //string fnWithoutExt = Path.GetFileNameWithoutExtension(srcFullName);
             string ext = Path.GetExtension(srcFullName);
-            string desFullName = srcFullName.Replace(ext, ".bak" + ext);
+            string desFullName = srcFullName.Replace(ext, $"_bak_{action}_{DateTime.Now:yyyyMMddHHmmssfff}_" + ext);
+            return desFullName;
+        }
+
+
+        private void CopyFile(string srcFullName, string desFullName)
+        {
             if (!File.Exists(desFullName))
             {
                 try
@@ -70,15 +94,18 @@ new Action(() =>
         private void Watcher_Created(object sender, FileSystemEventArgs e)
         {
             string srcFullName = e.FullPath;
-            if (srcFullName.Contains(".bak."))
+            if (srcFullName.Contains("_bak_"))
                 return;
-
+            if (new FileInfo(srcFullName).Length <= 0)
+                return;
             this.txtCurrentFileName.Dispatcher.Invoke(DispatcherPriority.Normal,
      new Action(() =>
      {
          txtCurrentFileName.Text = srcFullName;
      }));
-            CopyFile(srcFullName);
+            string desFullName = ConstructDestFullName(srcFullName, "created");
+
+            CopyFile(srcFullName, desFullName);
 
         }
     }
